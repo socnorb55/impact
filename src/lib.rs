d198897974df505217eadf9be65ec326a713d8b6
Keelan Zeigler <socnorb55@gmail.com>
2024-08-31 08:53:28 -0400

    ci: adding workflow and dependabot files

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub struct Request {
     recycling_types: Vec<String>,
     utility_usage: UtilityUsage,
     vehicles: Vec<Vehicles>,
-    zip_code: i32
+    zip_code: i32,
 }
 
 #[derive(Serialize)]
@@ -21,32 +21,34 @@ struct UtilityUsage {
     electricity: f32,
     fuel_oil: f32,
     natural_gas: f32,
-    propane: f32
+    propane: f32,
 }
 
 #[derive(Deserialize)]
 struct Vehicles {
     maintenance: bool,
     mpg: f32,
-    weekly_miles: i32
+    weekly_miles: i32,
 }
 
 pub fn calculate_carbon_footprint(event: LambdaEvent<Request>) -> Response {
-
-    let utility_co2_output: f32 = calculate_utility_co2_output(event.payload.utility_usage, event.payload.zip_code);
+    let utility_co2_output: f32 =
+        calculate_utility_co2_output(event.payload.utility_usage, event.payload.zip_code);
 
     let vehicle_co2_output: f32 = calculate_vehicle_co2_output(event.payload.vehicles);
 
-    let waste_co2_output: f32 = calculate_waste_co2_output(event.payload.household_member_count, event.payload.recycling_types);
+    let waste_co2_output: f32 = calculate_waste_co2_output(
+        event.payload.household_member_count,
+        event.payload.recycling_types,
+    );
 
     Response {
         req_id: event.context.request_id,
         carbon_footprint: 31931.0,
-    } 
+    }
 }
 
 fn calculate_utility_co2_output(utility_usage: UtilityUsage, zip_code: i32) -> f32 {
-
     let mut utility_co2_output: f32 = 0.0;
     let emission_factor: f32 = 1.0795718;
     println!("{}", zip_code);
@@ -63,39 +65,38 @@ fn calculate_utility_co2_output(utility_usage: UtilityUsage, zip_code: i32) -> f
 }
 
 fn calculate_vehicle_co2_output(vehicles: Vec<Vehicles>) -> f32 {
-
     let mut vehicle_co2_output: f32 = 0.0;
 
     for vehicle in vehicles {
-
         let vehicle_efficiency_output: f32 = match vehicle.maintenance {
             false => 0.04,
-            true => 1.0
+            true => 1.0,
         };
 
-        vehicle_co2_output += (vehicle.weekly_miles as f32 * 52.0) / vehicle.mpg * 19.6 * 1.01 * vehicle_efficiency_output
-    };
+        vehicle_co2_output += (vehicle.weekly_miles as f32 * 52.0) / vehicle.mpg
+            * 19.6
+            * 1.01
+            * vehicle_efficiency_output
+    }
 
     vehicle_co2_output
-
 }
 
 fn calculate_waste_co2_output(household_count: i32, recycling_types: Vec<String>) -> f32 {
-
     let mut recycling_reduction_output: f32 = 0.0;
 
     for recycling_type in recycling_types {
-
         match recycling_type.as_str() {
-            "aluminum and steel cans" => recycling_reduction_output += household_count as f32 * 89.38,
+            "aluminum and steel cans" => {
+                recycling_reduction_output += household_count as f32 * 89.38
+            }
             "plastic" => recycling_reduction_output += household_count as f32 * 35.56,
             "glass" => recycling_reduction_output += household_count as f32 * 25.39,
             "newspaper" => recycling_reduction_output += household_count as f32 * 113.14,
             "magazines" => recycling_reduction_output += household_count as f32 * 27.46,
-            _ => recycling_reduction_output += 0.0
+            _ => recycling_reduction_output += 0.0,
         };
     }
-
 
     household_count as f32 * 692.0 - recycling_reduction_output
 }
@@ -103,13 +104,8 @@ fn calculate_waste_co2_output(household_count: i32, recycling_types: Vec<String>
 #[cfg(test)]
 mod tests {
     use crate::{
-        calculate_carbon_footprint,
-        calculate_utility_co2_output,
-        calculate_vehicle_co2_output,
-        calculate_waste_co2_output,
-        Request,
-        UtilityUsage,
-        Vehicles
+        calculate_carbon_footprint, calculate_utility_co2_output, calculate_vehicle_co2_output,
+        calculate_waste_co2_output, Request, UtilityUsage, Vehicles,
     };
     use lambda_runtime::{Context, LambdaEvent};
 
@@ -124,19 +120,19 @@ mod tests {
             electricity: 170.0,
             fuel_oil: 0.0,
             natural_gas: 25.0,
-            propane: 0.0
+            propane: 0.0,
         };
 
         let vehicle_1: Vehicles = Vehicles {
             maintenance: true,
             mpg: 27.0,
-            weekly_miles: 50
+            weekly_miles: 50,
         };
 
         let vehicle_2: Vehicles = Vehicles {
             maintenance: true,
             mpg: 23.0,
-            weekly_miles: 150
+            weekly_miles: 150,
         };
 
         let payload = Request {
@@ -144,7 +140,7 @@ mod tests {
             recycling_types: vec![],
             utility_usage: utilities,
             vehicles: vec![vehicle_1, vehicle_2],
-            zip_code: 22079
+            zip_code: 22079,
         };
         let event: LambdaEvent<Request> = LambdaEvent { payload, context };
 
@@ -156,12 +152,11 @@ mod tests {
 
     #[test]
     fn test_calculate_utility_co2_output() {
-
         let utilities: UtilityUsage = UtilityUsage {
             electricity: 170.0,
             fuel_oil: 0.0,
             natural_gas: 25.0,
-            propane: 0.0
+            propane: 0.0,
         };
 
         let zip_code: i32 = 22079;
@@ -173,17 +168,16 @@ mod tests {
 
     #[test]
     fn test_calculate_vehicle_co2_output() {
-
         let vehicle_1: Vehicles = Vehicles {
             maintenance: true,
             mpg: 27.0,
-            weekly_miles: 50
+            weekly_miles: 50,
         };
 
         let vehicle_2: Vehicles = Vehicles {
             maintenance: true,
             mpg: 23.0,
-            weekly_miles: 150
+            weekly_miles: 150,
         };
 
         let response: f32 = calculate_vehicle_co2_output(vec![vehicle_1, vehicle_2]);
@@ -193,10 +187,8 @@ mod tests {
 
     #[test]
     fn test_calculate_waste_co2_output() {
-
         let response: f32 = calculate_waste_co2_output(2, vec![]);
 
         assert_eq!(response, 1384.0);
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use lambda_runtime::{service_fn, tracing, Error, LambdaEvent};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-
     tracing::init_default_subscriber();
 
     let func = service_fn(lambda_handler);
@@ -11,7 +10,8 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-pub(crate) async fn lambda_handler(event: LambdaEvent<impact::Request>) -> Result<impact::Response, Error> {
-
+pub(crate) async fn lambda_handler(
+    event: LambdaEvent<impact::Request>,
+) -> Result<impact::Response, Error> {
     Ok(impact::calculate_carbon_footprint(event))
 }
