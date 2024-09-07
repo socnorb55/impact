36bcca7911d668b7c983b5afad4706b783333132
socnorb55 <socnorb55@gmail.com>
2024-09-07 10:34:13 -0400

    feat: starting styling

--- frontend/src/app/home.rs
+++ frontend/src/app/home.rs
@@ -2,5 +2,7 @@ use yew::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
-    html! { <h1> {"Home"} </h1> }
+    html! {
+        <h1> {"Home"} </h1>
+    }
 }
--- frontend/src/app/mod.rs
+++ frontend/src/app/mod.rs
@@ -3,27 +3,32 @@ use yew_router::prelude::*;
 
 pub mod calculator;
 pub mod home;
+pub mod login;
 
 use calculator::Calculator;
 use home::Home;
-
+use login::Login;
+use crate::components::nav::Nav;
 
 #[derive(Clone, Routable, PartialEq)]
-enum Route {
+pub enum Route {
     #[at("/")]
     Home,
-    #[at("/calculator")]
-    Calculator,
     #[not_found]
     #[at("/404")]
     NotFound,
+    #[at("/calculator")]
+    Calculator,
+    #[at("/login")]
+    Login
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
         Route::Home => html! { <Home /> },
         Route::Calculator => html! { <Calculator /> },
-        Route::NotFound => html! { <h1>{ "404" }</h1> },
+        Route::Login => html! { <Login /> }
     }
 }
 
@@ -31,7 +36,10 @@ fn switch(routes: Route) -> Html {
 pub fn app() -> Html {
     html! {
         <BrowserRouter>
-            <Switch<Route> render={switch} />
+            <div class="bg-cosmic-latte text-eerie-black font-serif">
+                <Nav />
+                <Switch<Route> render={switch} />
+            </div>
         </BrowserRouter>
     }
 }
