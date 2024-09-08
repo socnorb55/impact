58f0271caf09100b191162e956433dc397aa62cc
socnorb55 <socnorb55@gmail.com>
2024-09-07 20:59:53 -0400

    feat: adding wireframes

--- frontend/src/app/login.rs
+++ frontend/src/app/login.rs
@@ -2,5 +2,15 @@ use yew::prelude::*;
 
 #[function_component(Login)]
 pub fn calculator() -> Html {
-    html! {}
+    html! {
+        <div class="login">
+            <form>
+                <label for="email">{ "Email Address" }</label><br />
+                <input id="email" type="text" name="email" /><br /><br />
+                <label for="password">{ "Password" }</label><br />
+                <input id="password" type="text" name="password" /><br /><br />
+                <input id="login_submit" type="submit" />
+            </form>
+        </div>
+    }
 }
